b0da9a8d23d7cb773d0db5a9b14f62deb5b1c737
devinle <devin.leggett@gmail.com>
2021-02-18 15:31:03 -0800

    With npm init setup

--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -3,38 +3,51 @@
 // StyleDictionaryColor.h
 //
 // Do not edit directly
-// Generated on Fri, 06 Nov 2020 20:33:02 GMT
+// Generated on Sat, 13 Feb 2021 00:08:18 GMT
 //
 
 #import <UIKit/UIKit.h>
 
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
-BordersUnsupportedMultipleBordersStroke,
-BordersSingleStroke,
-BordersSingleStyleStroke,
-BordersDashedOutsideStroke,
-ColorsMultipleFills0,
-ColorsMultipleFills1,
-ColorsSingleBlue,
-ColorsSpecialCharacters😅,
-ColorsSpecialCharactersAnderung,
-GradientMultiple0Stops0Color,
-GradientMultiple0Stops1Color,
-GradientMultiple1Stops0Color,
-GradientMultiple1Stops1Color,
-GradientMultiple2Stops0Color,
-GradientMultiple2Stops1Color,
-GradientMultiple3Stops0Color,
-GradientMultiple3Stops1Color,
-GradientSingleWithMultipleColorStopsStops0Color,
-GradientSingleWithMultipleColorStopsStops1Color,
-GradientSingleWithMultipleColorStopsStops2Color,
-GradientSingleWithMultipleColorStopsStops3Color,
-EffectDropShadowSingleColor,
-EffectInnerShadowMultiple0Color,
-EffectInnerShadowMultiple1Color,
-EffectInnerShadowMultiple2Color
+LightThemeBrandPrimary,
+LightThemeBrandSecondary,
+LightThemeBrandAccent,
+LightThemeBrandMuted,
+LightThemeUiPrimary,
+LightThemeUiSecondary,
+LightThemeUiTertiary,
+LightThemeUiQuaternary,
+LightThemeUiDisabled,
+LightThemeUiError,
+LightThemeUiSuccess,
+LightThemeBackground1,
+LightThemeBackground2,
+LightThemeTextPrimary,
+LightThemeTextSecondary,
+LightThemeTextDisabled,
+LightThemeTextInverse,
+LightThemeTextError,
+LightThemeTextSuccess,
+DarkThemeBrandPrimary,
+DarkThemeBrandSecondary,
+DarkThemeBrandAccent,
+DarkThemeBrandMuted,
+DarkThemeUiPrimary,
+DarkThemeUiSecondary,
+DarkThemeUiTertiary,
+DarkThemeUiQuaternary,
+DarkThemeUiDisabled,
+DarkThemeUiError,
+DarkThemeUiSuccess,
+DarkThemeBackground1,
+DarkThemeBackground2,
+DarkThemeTextPrimary,
+DarkThemeTextSecondary,
+DarkThemeTextDisabled,
+DarkThemeTextInverse,
+DarkThemeTextError,
+DarkThemeTextSuccess
 };
 
 @interface StyleDictionaryColor : NSObject
