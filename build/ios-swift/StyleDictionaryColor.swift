aeffd0322bf5d76186c6b3067e9a49e267e32601
Automated Publisher <actions@users.noreply.github.com>
2021-02-18 23:37:01 +0000

    Automated publish: Thu Feb 18 23:37:01 UTC 2021 14d0600f174200e2fe1ee8480dbf35ff034050dd

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -3,32 +3,12 @@
 // StyleDictionaryColor.swift
 //
 // Do not edit directly
-<<<<<<< HEAD
-// Generated on Tue, 16 Feb 2021 23:05:20 GMT
-=======
-<<<<<<< HEAD
-// Generated on Sat, 13 Feb 2021 00:08:18 GMT
-=======
-// Generated on Sun, 14 Feb 2021 00:36:40 GMT
->>>>>>> ef6c8afe35a2350efee3f6d34898b11f8507c62d
->>>>>>> main
+// Generated on Thu, 18 Feb 2021 23:37:00 GMT
 //
 
 import UIKit
 
 public enum StyleDictionaryColor {
-<<<<<<< HEAD
-=======
-    public static let dashedOutsideStroke = rgba(64, 255, 186, 1)
-    public static let singleStroke = rgba(112, 141, 242, 1)
-    public static let singleStyleStroke = rgba(4, 74, 255, 1)
-    public static let unsupportedMultipleBordersStroke = rgba(255, 230, 0, 1)
-    public static let multipleFills0 = rgba(64, 255, 186, 1)
-    public static let multipleFills1 = rgba(0, 0, 0, 0.1)
-    public static let singleBlue = rgba(4, 74, 255, 1)
-    public static let specialCharactersAnderung = rgba(52, 86, 175, 1)
-    public static let specialCharacters😅 = rgba(64, 223, 80, 1)
->>>>>>> ef6c8afe35a2350efee3f6d34898b11f8507c62d
     public static let background1 = rgba(17, 17, 17, 1)
     public static let background2 = rgba(38, 38, 38, 1)
     public static let brandAccent = rgba(253, 180, 71, 1)
@@ -48,25 +28,6 @@ public enum StyleDictionaryColor {
     public static let uiSecondary = rgba(161, 161, 161, 1)
     public static let uiSuccess = rgba(28, 189, 0, 1)
     public static let uiTertiary = rgba(60, 60, 60, 1)
-<<<<<<< HEAD
-=======
-    public static let dropShadowSingleColor = rgba(0, 0, 0, 0.25)
-    public static let innerShadowMultiple0Color = rgba(0, 0, 0, 0.25)
-    public static let innerShadowMultiple1Color = rgba(0, 0, 0, 1)
-    public static let innerShadowMultiple2Color = rgba(0, 0, 0, 0.25)
-    public static let multiple0Stops0Color = rgba(255, 184, 0, 1)
-    public static let multiple0Stops1Color = rgba(255, 184, 0, 1)
-    public static let multiple1Stops0Color = rgba(255, 255, 255, 1)
-    public static let multiple1Stops1Color = rgba(255, 255, 255, 1)
-    public static let multiple2Stops0Color = rgba(207, 48, 48, 1)
-    public static let multiple2Stops1Color = rgba(255, 255, 255, 1)
-    public static let multiple3Stops0Color = rgba(74, 79, 204, 1)
-    public static let multiple3Stops1Color = rgba(255, 255, 255, 1)
-    public static let singleWithMultipleColorStopsStops0Color = rgba(255, 184, 0, 1)
-    public static let singleWithMultipleColorStopsStops1Color = rgba(255, 138, 0, 1)
-    public static let singleWithMultipleColorStopsStops2Color = rgba(255, 46, 0, 1)
-    public static let singleWithMultipleColorStopsStops3Color = rgba(255, 0, 0, 1)
->>>>>>> ef6c8afe35a2350efee3f6d34898b11f8507c62d
     public static let background1 = rgba(255, 255, 255, 1)
     public static let background2 = rgba(241, 241, 241, 1)
     public static let brandAccent = rgba(219, 124, 0, 1)
